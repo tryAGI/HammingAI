a0c923c565b7a2fce2f9d87abafaf2a9c0260265
Konstantin S. <havendv@gmail.com>
2026-05-07 16:33:50 +0400

    fix: restore local SDK build

---
+++ src/libs/HammingAI/Extensions/PromptWithContent.Compatibility.cs
@@ -0,0 +1,11 @@
+#nullable enable
+#pragma warning disable CS1591
+
+namespace HammingAI;
+
+public readonly partial struct PromptWithContent
+{
+    public Prompt? Value1 => Prompt;
+
+    public PromptWithContentVariant2? Value2 => PromptWithContentVariant2;
+}
